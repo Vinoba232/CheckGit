d45a65b692cb6feea2d9c76b2c8b0fb046b9b1bf
Vinoba232 <82799201+Vinoba232@users.noreply.github.com>
2021-05-10 11:09:52 -0700

    Add files via upload

---
+++ Assessment3/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LoginPage
+{
+    class Program
+    {
+        string username, password;
+        public void GettingUsernameAndPassword()
+        {           
+                Console.WriteLine("Please enter the username");
+                username = Console.ReadLine();
+                Console.WriteLine("Please enter the password");
+                password = Console.ReadLine();       
+                
+        }
+        void Checking()
+        {
+            string myUsername = "Admin";
+            string myPassword = "admin";
+            int count = 1;
+            do
+            {
+                GettingUsernameAndPassword();
+                if(username==myUsername && password == myPassword)
+                {
+                    Console.WriteLine("Welcome!!!");
+                    break;
+                }
+                else
+                {
+                    if (count == 3)
+                    {
+                        Console.WriteLine("Sorry..You have already tried 3 times.");
+                        break;
+                    }
+                    Console.WriteLine("Invalid Username or Password...\nTry Again ");
+                    count++;                   
+                }
+            } while (count<=3);
+            
+        }
+        static void Main(string[] args)
+        {
+            new Program().Checking();
+        }
+    }
+}
